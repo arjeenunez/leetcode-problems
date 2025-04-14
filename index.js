240d88f9fcdf813dde216605a2a23387e45bc361
Arjee <arjeenunez@gmail.com>
2025-04-15 00:48:56 +0800

    Solved 1534

--- index.js
+++ index.js
@@ -3393,7 +3393,26 @@
 
 // reverseDegree("abc");
 
+// 1534. Count Good Triplets
 
+// const countGoodTriplets = function(arr, a, b, c) {
+//     let result = 0;
+//     for (let i = 0; i < arr.length; i++) {
+//         for (let j = i + 1; j < arr.length; j++) {
+//             let check1 = Math.abs(arr[i] - arr[j]) > a;
+//             if (check1) continue;
+//             for (let k = j + 1; k < arr.length; k++) {
+//                 let check2 = Math.abs(arr[j] - arr[k]) > b;
+//                 let check3 = Math.abs(arr[i] - arr[k]) > c;
+//                 if (check2 || check3) continue;
+//                 result++;
+//             }
+//         }
+//     }
+//     return result;
+// };
+
+// countGoodTriplets([3, 0, 1, 1, 9, 7], 7, 2, 3);
 
 
 

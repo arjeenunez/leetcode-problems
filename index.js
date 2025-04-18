45afa24b736c0241dd66740d3f5792a8adb390e9
Arjee <arjeenunez@gmail.com>
2025-04-19 01:33:28 +0800

    Solved 503

--- index.js
+++ index.js
@@ -3741,7 +3741,23 @@
 
 // nextGreaterElement([4, 1, 2], [1, 3, 4, 2]);
 
+// 503. Next Greater Element II
 
+// const nextGreaterElements = function(nums) {
+//     let n = nums.length;
+//     const stack = [];
+//     const result = new Array(n).fill(-1);
+//     for (let i = n * 2 - 1; i >= 0; i--) {
+//         while (stack.length && stack.at(-1) <= nums[i % n]) {
+//             stack.pop();
+//         }
+//         if (stack.length) result[i % n] = stack.at(-1);
+//         stack.push(nums[i % n]);
+//     }
+//     return result;
+// };
+
+// nextGreaterElements([1, 2, 1]);
 
 
 

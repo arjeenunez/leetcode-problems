141058177ef6b87fc7804db7cd3c4188e8c47bde
Arjee <arjeenunez@gmail.com>
2025-04-18 16:37:36 +0800

    Solved 496

--- index.js
+++ index.js
@@ -3722,8 +3722,24 @@
 
 // isAlienSorted(["hello", "leetcode"], "hlabcdefgijkmnopqrstuvwxyz");
 
+// 496. Next Greater Element I
 
+// const nextGreaterElement = function(nums1, nums2) {
+//     const hash = new Map();
+//     for (let i = 0; i < nums2.length; i++) {
+//         for (let j = i + 1; j <= nums2.length; j++) {
+//             if (nums2[j] > nums2[i]) {
+//                 hash.set(nums2[i], nums2[j]);
+//                 break;
+//             }
+//             hash.set(nums2[i], -1);
+//         }
+//     }
+//     for (let i = 0; i < nums1.length; i++) nums1[i] = hash.get(nums1[i]);
+//     return nums1;
+// };
 
+// nextGreaterElement([4, 1, 2], [1, 3, 4, 2]);
 
 
 

f733a6b4cefe9d12b3715e69555436f452a115ec
Arjee <arjeenunez@gmail.com>
2025-05-03 16:00:40 +0800

    Solved 2248

--- index.js
+++ index.js
@@ -3974,6 +3974,21 @@
 
 // findDifference([1, 2, 3], [2, 4, 6]);
 
+// 2248. Intersection of Multiple Arrays
+
+// const intersection = function(nums) {
+//     let setDiff = new Set(nums[0]);
+//     for (let i = 1; i < nums.length; i++) {
+//         let setB = new Set(nums[i]);
+//         setDiff = setDiff.intersection(setB);
+//     }
+//     return [...setDiff].sort((a, b) => a - b);
+// };
+
+// intersection([[3, 1, 2, 4], [2, 3], [2]]);
+// intersection([[1, 2, 3], [4, 5], [6]]);
+
+
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // RECURSIVE FUNCTION PROBLEMS
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
